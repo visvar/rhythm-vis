838ec16bc32362c94b3ce6c9d899e44d4f0c0d90
fheyen <frank.heyen@outlook.de>
2024-02-23 15:38:57 +0100

    add erros and concat correct and wrong pattern

--- rhythm-audio-vis-jnd-drums/src/Playground.svelte
+++ rhythm-audio-vis-jnd-drums/src/Playground.svelte
@@ -8,126 +8,183 @@
   import { Utils } from 'musicvis-lib';
   import { onMount } from 'svelte';
 
-  const audioFiles = [
-    './bass.mp3',
-    './bass2.mp3',
-    './snare.mp3',
-    './snare2.mp3',
-    './hihat.mp3',
-  ];
-
   // vis sizes
   let visWidth = 600;
   let visHeight = 100;
 
-  // inter-onset interval in seconds
-  // let ioi = 0.5;
-  // $: bpm = (1 / ioi) * 60;
-  // // deviation in percent of the ioi
-  // let percentDeviation = 5;
-  // // deviation in seconds
-  // $: deviationSeconds = (percentDeviation / 100) * ioi;
-
-  // let noteCount = 6;
-  // let wrongNoteIndex = 3;
-  // let shiftFollowing = true;
-  // let paddingStart = 0;
-
-  // // generate rhythmic pattern
-  // let pattern = [];
-  // $: {
-  //   pattern = generatePatternSimple(
-  //     noteCount,
-  //     ioi,
-  //     wrongNoteIndex,
-  //     deviationSeconds,
-  //     shiftFollowing,
-  //     paddingStart,
-  //   );
-  //   // data = simulate(instrument, pattern)
-  //   console.log('generated pattern', pattern);
-  // }
-
   let BPM = 120;
+
+  /**
+   * @type {'single note'|'only snare'|'single beat'}
+   */
+  let errorMode = 'single note';
+  let errorSeverity = 0;
+
   // @ts-ignore
   let spb = Utils.bpmToSecondsPerBeat(BPM);
 
-  let drumPattern = [
+  let correctPattern = [
     {
+      instrument: 'bass',
       sample: './bass.mp3',
       beats: [1, 3],
     },
     {
+      instrument: 'snare',
       sample: './snare.mp3',
       beats: [2, 4],
     },
     {
+      instrument: 'hihat',
       sample: './hihat.mp3',
       beats: [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5],
     },
   ];
-  const duration = 4 * spb;
+  const beats = 4;
+  const duration = beats * spb;
 
   let renderedAudio = null;
   let sampleRate;
   let noteTimes = [];
 
-  const createDrumPattern = async () => {
-    drumPattern = await Promise.all(
-      drumPattern.map(async (d) => {
+  const fetchDrumAudios = async () => {
+    correctPattern = await Promise.all(
+      correctPattern.map(async (d) => {
         return {
           ...d,
           times: d.beats.map((b) => spb * (b - 1)),
+          timesOriginal: d.beats.map((b) => spb * (b - 1)),
           audioBuffer: await fetchAudio(d.sample),
         };
       }),
     );
-    noteTimes = drumPattern
+  };
+
+  /**
+   * Adds errors based on mode and severity
+   * @param {'single note'|'only snare'|'single beat'} errorMode
+   * @param {number} errorSeverity
+   */
+  const addErrors = (errorMode = 'single note', errorSeverity = 0) => {
+    if (errorSeverity === 0) {
+      return correctPattern;
+    }
+    console.log(`Adding error ${errorMode} ${errorSeverity}`);
+    const copy = concat([correctPattern]);
+
+    const snare = copy.filter((d) => d.instrument === 'snare')[0];
+    const hihat = copy.filter((d) => d.instrument === 'hihat')[0];
+    if (errorMode === 'single note') {
+      // take the second snare
+      snare.times[2 - 1] = snare.timesOriginal[2 - 1] + errorSeverity;
+    } else if (errorMode === 'single beat') {
+      // take the second snare and 7th hihat
+      snare.times[2 - 1] = snare.timesOriginal[2 - 1] + errorSeverity;
+      hihat.times[7 - 1] = hihat.timesOriginal[7 - 1] + errorSeverity;
+    } else if (errorMode === 'only snare') {
+      // take first and second snare
+      snare.times[1 - 1] = snare.timesOriginal[1 - 1] + errorSeverity;
+      snare.times[2 - 1] = snare.timesOriginal[2 - 1] + errorSeverity;
+    }
+    // console.log(drumPattern);
+    return copy;
+  };
+
+  /**
+   * Concats multiple drum patterns
+   * @param object[] patterns
+   */
+  const concat = (patterns) => {
+    console.log(`concat ${patterns.length} patterns`);
+    const result = patterns[0].map((d) => {
+      return {
+        ...d,
+        beats: [...d.beats],
+        times: [...d.times],
+        timesOriginal: [...d.timesOriginal],
+      };
+    });
+    for (let i = 1; i < patterns.length; i++) {
+      const pattern = patterns[i];
+      for (const inst of pattern) {
+        const instr = result.filter((d) => d.instrument === inst.instrument)[0];
+        instr.beats = [...instr.beats, ...inst.beats.map((b) => b + i * beats)];
+        instr.times = [
+          ...instr.times,
+          ...inst.times.map((b) => b + i * duration),
+        ];
+        instr.timesOriginal = [
+          ...instr.timesOriginal,
+          ...inst.timesOriginal.map((b) => b + i * duration),
+        ];
+      }
+    }
+    return result;
+  };
+
+  /**
+   * Creates the pattern with errors and repetitions
+   * @param errorMode
+   * @param errorSeverity
+   */
+  const createDrumPattern = async (errorMode, errorSeverity) => {
+    if (!correctPattern[0].audioBuffer) {
+      return;
+    }
+    // add errors
+    const wrongPattern = addErrors(errorMode, errorSeverity);
+    // concat correct and the one with errors
+    const patterns = [correctPattern, wrongPattern];
+    const combinedPattern = concat(patterns);
+    // get times for visualizations
+    noteTimes = combinedPattern
       .map((d) => d.times)
       .flat()
       .sort();
-
-    sampleRate = drumPattern[0].audioBuffer.sampleRate;
-    // console.log(drumPattern);
-
+    // set global sample rate
+    sampleRate = combinedPattern[0].audioBuffer.sampleRate;
     // render audio
-    renderedAudio = simulateDrum(drumPattern, duration, sampleRate);
-    // console.log(renderedAudio);
+    renderedAudio = simulateDrum(
+      combinedPattern,
+      duration * patterns.length + 0.1,
+      sampleRate,
+    );
   };
 
-  onMount(createDrumPattern);
+  // do in the beginning
+  onMount(async () => {
+    await fetchDrumAudios();
+    createDrumPattern();
+  });
+
+  // and also when error settings change
+  $: {
+    createDrumPattern(errorMode, errorSeverity);
+  }
 </script>
 
 <main>
   <h2>Playground</h2>
-  <!-- <p>
-    All stimuli (audio and visualizations) and configurable deviation. Play
-    around with different values to compare how stimuli look and how small
-    deviations can be
-  </p>
   <div>
-    inter-onset interval (IOI): {(ioi * 1000).toFixed(0)} milliseconds ({bpm} bpm)
     <label>
-      deviation as percent of IOI
-      <input
-        type="range"
-        bind:value="{percentDeviation}"
-        min="-20"
-        max="20"
-        step="1"
-      />
-      {percentDeviation.toFixed(1)}
-      ({(deviationSeconds * 1000).toFixed(1)} milliseconds)
-    </label>
-    <label>
-      instrument sample:
-      <select bind:value="{audioFile}">
-        {#each audioFiles as af}
+      error mode:
+      <select bind:value="{errorMode}">
+        {#each ['single note', 'only snare', 'single beat'] as af}
           <option value="{af}">{af.substring(0, 30)}</option>
         {/each}
       </select>
     </label>
-  </div> -->
+    <label>
+      error severity (in seconds):
+      <input
+        bind:value="{errorSeverity}"
+        type="number"
+        min="-0.5"
+        max="0.5"
+        step="0.01"
+      />
+    </label>
+  </div>
 
   <div class="stimuli">
     <Audio audioData="{renderedAudio}" {sampleRate} />
